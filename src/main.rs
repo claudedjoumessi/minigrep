d6221267465ad73fe7dbab276efd3e075dcfb701
Claude Djoumessi <djoumessiclaude95@gmail.com>
2025-08-27 15:19:02 +0100

    hotfix: implement `new` to create a `Config` impl instance and argument panic message

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    // let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Query is: {}", config.query);
     println!("File path is: {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read file");
 
     println!("With contents\n: {contents}")
 }
@@ -18,10 +20,16 @@ struct Config {
     file_path: String,
 }
 
-/// Config parser
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+/// Config instance
+impl Config {
+    /// Generates a new `Config` object from args
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Not enough arguments!. Exiting...");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
-    return Config { query, file_path };
+        return Config { query, file_path };
+    }
 }

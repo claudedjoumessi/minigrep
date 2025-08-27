cf16b4a3692a581077b34d71e40853f7ae8b3b54
Claude Djoumessi <djoumessiclaude95@gmail.com>
2025-08-27 18:54:05 +0100

    chrore: write basic (failed) test for `search`

--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,26 @@ impl Config {
         Ok(Config { query, file_path })
     }
 }
+
+/// Tests
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  /// Search Test
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+  }
+}
+
+/// Search functionality
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  vec![]
+}

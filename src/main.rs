30cfb8b5e33df689f8e357648770e4293f2cd58d
Claude Djoumessi <djoumessiclaude95@gmail.com>
2025-08-27 15:50:29 +0100

    feat: improved on error handling with `unwrap_or_else` closure

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
-    // let config = parse_config(&args);
-    let config = Config::new(&args);
+    
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Oops! Grepped into an error: {}", err);
+        process::exit(1); // Exitting with non-zero code shows potential error in exec
+    });
 
     println!("Query is: {}", config.query);
     println!("File path is: {}", config.file_path);
@@ -22,14 +24,15 @@ struct Config {
 
 /// Config instance
 impl Config {
-    /// Generates a new `Config` object from args
-    fn new(args: &[String]) -> Config {
+    /// Builds a new `Config` object from args
+    /// and throws an `Err` in case `args.len() < 3`
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments!. Exiting...");
+            return Err("Not enough arguments!. Exiting...");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        return Config { query, file_path };
+        Ok(Config { query, file_path })
     }
 }

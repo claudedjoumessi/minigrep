68bf07bdb60935d9f3c462c164cff1dc183b5914
Claude Djoumessi <djoumessiclaude95@gmail.com>
2025-08-27 15:00:19 +0100

    fix: created config struct and refactored `parse_config` accordingly

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,25 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    println!("Query is: {query}");
-    println!("File path is: {file_path}");
+    println!("Query is: {}", config.query);
+    println!("File path is: {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read file");
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read file");
 
     println!("With contents\n: {contents}")
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+/// Config parser
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    return Config { query, file_path };
 }
